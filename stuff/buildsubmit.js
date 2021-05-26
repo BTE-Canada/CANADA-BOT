d69ce6129f2b3c0bbe98e9a2919d15a7e5ccef2b
Celery <the.real.paintblob@gmail.com>
2021-05-26 14:03:34 -0400

    update build submit msgs

--- stuff/buildsubmit.js
+++ stuff/buildsubmit.js
@@ -3,14 +3,6 @@ const { pingChannel } = require('../config.json')
 
 async function run(message, client, con) {
     const msgChannel = await client.channels.fetch(pingChannel);
-
-    //build submit
-    if (message.content.includes('submitted a build')) {
-        const sentence = message.content.split(': ')
-        const words = sentence[1].split(' ')
-        const user = words[0]
-        msgChannel.send(user + ' submitted a bJuild!')
-    }
     //build approved
     if (message.content.includes('build approve') || message.content.includes('build deny')) {
         const sentence = message.content.split('/')
@@ -29,32 +21,39 @@ async function run(message, client, con) {
         let userId = ''
         con.query(`SELECT * FROM discordsrv_accounts WHERE uuid = '${fullUuid}'`, async function (err, result, fields) {
             if (err) throw err;
-
-            if (result == undefined) return;
+            if (result == undefined) return msgChannel.send('User was not found in the database.');
             userId = result[0].discord
 
-            msgChannel.send(`<@${userId}> is now a builder!`);
+            //build approved
+            if (message.content.includes('build approve')) {
+                msgChannel.send(`<@${userId}> is now a builder!`);
 
-            const user = await client.users.fetch(userId)
+                const user = await client.users.fetch(userId)
+                const dm = await user.createDM()
+                dm.send('Your build was approved. Congrats, you are now a builder!')
 
-            //DM user
-            const dm = await user.createDM()
-            dm.send('Your build was approved. Congrats, you are now a builder!')
+                //update to builder role
+                const member = await message.guild.members.fetch(userId)
+                member.roles.add('692801758761844746')
+                member.roles.remove('692802742200172634')
+            };
 
-            //update to builder role
-            const member = await message.guild.members.fetch(userId)
-            member.roles.add('692801758761844746')
-            member.roles.remove('692802742200172634')
-        });
+            //build build denied
+            if (message.content.includes('build deny')) {
+                msgChannel.send(`<@${userId}> got rejected!`)
+                const dm = await user.createDM()
+                dm.send('Your build was denied. Take a look at the feedback, then improve your build and submit it again!')
+            }
+        })
     }
 
-    /* //build deny
-     if (message.content.includes('denied')) {
-         const sentence = message.content.split(': ')
-         const words = sentence[1].split(' ')
-         const user = words[0]
-         msgChannel.send(user + ' got rejected!')
-     }*/
+    //build submit
+    if (message.content.includes('submitted a build')) {
+        const sentence = message.content.split(': ')
+        const words = sentence[1].split(' ')
+        const user = words[0]
+        msgChannel.send(user + ' submitted a bJuild!')
+    }
 }
 
 module.exports = run
