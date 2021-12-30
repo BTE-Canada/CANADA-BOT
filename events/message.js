241135befd0f17643bc3ca6a91f6f3ca914f16ef
dy-dx-1 <nic19sarbe@gmail.com>
2021-12-30 11:57:29 -0500

    Added msg event handling -> how join & is finished

--- events/message.js
+++ events/message.js
@@ -3,13 +3,41 @@ const fs = require('fs')
 const Discord = require('discord.js')
 const cooldowns = new Discord.Collection()
 
+function check_if_in_message(string_check, words_check) { // string contains the words to check, separated by spaces 
+    const array_words = words_check.split(" ") 
+    for (const word of array_words) {
+        if (!(string_check.includes(word))) {
+            return false
+        }
+        else {
+            // pass 
+        }
+    }
+    return true
+} 
 module.exports = {
     name: 'message',
     async execute(message, client, con2) {
         if (message.author.bot) return
-        if (message.content == "uwuusowarm" && message.author.id =="360392861608574978") { 
+
+        // Message events // 
+        if (message.content == "uwuusowarm" && message.author.id =="360392861608574978") {  // Ignore this : ) 
             message.reply(":smirk_cat:") 
         }
+        if (check_if_in_message("how join", message.content.toLowerCase()) && (!message.author.roles.find(role => role.name === "Builder"))) {
+            message.reply(`
+            "Hewo fwend :3, I ***suspect*** :jesus: that you may be looking for how to join our server :ahegao:,
+            :pleading_face: pwease :pleading_face: look in <#752648404219461753> and <#776176449849393162> to find how to do it, welcome to BTE-Canada, rawr! :SPINNYCANADA:`) 
+        }
+        const basic_msg_re = new RegExp(".*is(\s{1,2}\w+\s{1,2}|\s)finished.*")
+        if (basic_msg_re.test(message.content.toLowerCase())) {
+            message.reply(`:heart_eyes_cat: Ooooohhhhhhhh are you :thinking: wondering :thinking: if *[insert city]* is finished :pleading_face:?\n)
+            It certainly isn't! :heart_eyes_cat: Guess what, we need ***your*** help to finish it :smirk_cat:. 
+            W-w-w-wait, senpai are you thinking that you are not a good enough builder to help us????? :pensive: :crying_cat_face: ...  
+            Let me tell you that ***you are wrong!*** We need people of all skill levels, we all sucked when we started, and I promise you that you'll get
+            the hang of this with practice, so, what are you waiting for? Go <#752648404219461753>!!!! :weary: *NYA!* :smile_cat:`)
+        }
+        // Random celery js stuff // 
         if (!prefix.includes(message.content.charAt(0))) {
             return
         }
