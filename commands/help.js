22705276b720aa21fce689f8e942afa9fddb5762
celery6 <soccercli12@gmail.com>
2022-01-06 14:40:50 -0600

    updates for v13

--- commands/help.js
+++ commands/help.js
@@ -4,11 +4,13 @@ module.exports = {
     name: 'help',
     needAdmin: false,
     execute(msg, args, client) {
-        if (args[0] == 'points') {
+        if (args[0] == 'me') {
             return msg.reply('you need help')
         }
         if (msg.member.roles.cache.has('812569861317459968')) {
-            msg.reply('you have staff role.\n\ngive points to a build submission: `=review [submission link] [S/M/L] [quality 1, 1.5, 2] <incompletion 0.5, 1> <collaborators>`\n\nedit the points for a build submission:`=edit [submission link] [S/M/L] [quality 1, 1.5, 2] <incompletion 0.5, 1> <collaborators>`\n\nsend feedback for a build submission via DM: `=feedback [submission msg link] [feedback feedback]`')
+            msg.reply(
+                'you have staff role.\n\ngive points to a build submission: `=review [submission link] [S/M/L] [quality 1, 1.5, 2] <incompletion 0.5, 1> <collaborators>`\n\nedit the points for a build submission:`=edit [submission link] [S/M/L] [quality 1, 1.5, 2] <incompletion 0.5, 1> <collaborators>`\n\nsend feedback for a build submission via DM: `=feedback [submission msg link] [feedback feedback]`'
+            )
         }
         msg.channel.send(
             `I use the following prefixes:\n` +
