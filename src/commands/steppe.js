d64bc6dea78ce53b36b8ac3d3b02807c1e55e149
dy-dx-1 <nic19sarbe@gmail.com>
2022-01-19 14:52:27 -0500

    newq command steppe

---
+++ src/commands/steppe.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'steppe',
+    aliases: ['step', 'poem'], 
+    cooldown: 10,
+    needAdmin: false,
+    execute(msg, args, client) {
+        if (msg.author.id == '685605107378946057' || msg.author.id == '360392861608574978') {
+            msg.channel.send(`steppe
+steppe
+steppe 
+stop
+stoppe
+good bye steppes
+i must travel the interwebs
+[picture]
+No more steppe jesus
+:suh_huh:
+:xeram:
+steppe poem`)
+        }
+        else {
+            msg.channel.send("You are not allowed to use this.")
+        }
+    },
+} // cry about the formatting 
