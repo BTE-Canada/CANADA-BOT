2414fe84cc81cca0b271bbf1cfc06230b1fec4b7
celery6 <soccercli12@gmail.com>
2022-03-29 17:24:14 -0500

    gep update

--- src/commands/gep.js
+++ src/commands/gep.js
@@ -3,8 +3,9 @@ module.exports = {
     cooldown: 1,
     needAdmin: false,
     execute(msg, args, client) {
-        msg.channel.send(
-            '**Download:** https://www.google.com/earth/versions/download-thank-you/?usagestats=0 \n \n**Ruler Tool (useful for measuring horizontal lengths like windows):** https://gyazo.com/d58446cec35cc504bb36b749346041a9\n\n**Elevation (useful for measuring vertical distances like house heights)** is in the bottom right corner. Use this to measure building heights by calculating the difference in elevation between the ground and the top of a building.\n\nPress CTRL+SHIFT+C to automatically **copy coordinates** of where your mouse is hovering.'
-        )
+        msg.channel.send({
+            content:
+                '**Download:** https://www.google.com/earth/versions/download-thank-you/?usagestats=0 \n \n**Ruler Tool (useful for measuring horizontal lengths like windows):** https://gyazo.com/d58446cec35cc504bb36b749346041a9\n\n**Elevation (useful for measuring vertical distances like house heights)** of where your mouse is hovering is shown in the bottom right corner. Use this to measure building heights by calculating the difference in elevation between the ground and the top of a building.\n\nPress CTRL+SHIFT+C to automatically **copy coordinates** of where your mouse is hovering.',
+        })
     },
 }
