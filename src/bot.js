a42d5000e11938f2b939c6ddceb38625af609d9e
Noah Tomkins <1tomkinsnoa@gmail.com>
2022-08-21 23:44:29 -0400

    Moddifed

--- src/bot.js
+++ src/bot.js
@@ -11,32 +11,7 @@ const client = new Client({
 })
 
 async function run() {
-    db(client)
 
-    // normal commands
-    client.commands = new Collection()
-    const commandFiles = fs
-        .readdirSync('./commands')
-        .filter((file) => file.endsWith('.js'))
-
-    for (const file of commandFiles) {
-        const command = require(`./commands/${file}`)
-        client.commands.set(command.name, command)
-    }
-
-    // register events
-    const eventFiles = fs
-        .readdirSync('./events')
-        .filter((file) => file.endsWith('.js'))
-
-    for (const file of eventFiles) {
-        const event = require(`./events/${file}`)
-        if (event.once) {
-            client.once(event.name, (...args) => event.execute(...args, client))
-        } else {
-            client.on(event.name, (...args) => event.execute(...args, client))
-        }
-    }
 }
 
 client.login(token)
